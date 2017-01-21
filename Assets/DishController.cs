a6a9799019518233e9989902bf3d327d40419eb7
CameronLeifJacobson <cameronleifjacobson@hotmail.com>
2017-01-21 14:29:37 -0800

    Added dishes to SETI2!

--- Assets/DishController.cs
+++ Assets/DishController.cs
@@ -2,10 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class POIArbiter : MonoBehaviour {
+public class DishController : MonoBehaviour {
 
-    public POIController[] POIControllers;
-    
 	// Use this for initialization
 	void Start () {
 		
